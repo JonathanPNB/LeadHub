295b3e15a561e2a6107508bf9e8a198bc1a2bf52
Jonathan <jonathan.pronoob@gmail.com>
2026-07-27 22:54:28 -0300

    fix request jtimob leads

--- src/jetimob/leads.js
+++ src/jetimob/leads.js
@@ -1,8 +1,4 @@
-import express from "express";
 import { dataHora } from "../util/util.js";
-
-const app = express();
-app.use(express.json());
 
 //Busca os contatos já cadastrados no Jetmobi
 export async function getJetimobLeads() {
@@ -28,36 +24,3 @@ export async function getJetimobLeads() {
     console.error('Erro ao buscar leads:', error.message);
   }
 }
-
-// Rota GET com Try/Catch
-app.get('/jetimob/leads', async (req, res) => {
-  try {
-    // Requisição ao Supabase
-    const { data, error } = await supabase
-      .from('Contatos_JetiMob')
-      .select('*');
-
-    // Verifica se o Supabase retornou um erro de banco/regra
-    if (error) {
-      console.error("[server.js] error.message: "+error.message);
-      console.error("[server.js] error.details: "+error.details);
-      return res.status(400).json({ 
-        sucesso: false, 
-        error: error.message,
-        details: error.details
-      });
-    }
-    
-    console.log("[server.js] dados: "+data);
-    // Retorno de sucesso
-    res.json({ sucesso: true, dados: data });
-
-  } catch (err) {
-    // Captura erros críticos (ex: rede, crash do servidor, variáveis nulas)
-    console.error('Erro interno no servidor:', err);
-    res.status(500).json({ 
-      sucesso: false, 
-      erro: 'Erro interno no servidor ao buscar dados.' 
-    });
-  }
-});
