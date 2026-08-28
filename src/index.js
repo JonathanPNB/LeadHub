2c886ca71b8de309211fcbf9c279c6d0d6646078
Jonathan <jonathan.pronoob@gmail.com>
2026-08-27 22:44:39 -0300

    fix order execution

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import { dataHora } from "./util/util.js";
 import { syncChatproEventos } from "./chatpro/sync.js";
 import { inserirLeadsJetiMob } from "./jetimob/leads.js"
 
-inserirLeadsJetiMob().catch((error) => {
-  console.error(`[${dataHora()}][index.js] Falha ao buscar Leads: ${error.message}`);
-  process.exitCode = 1;
-});
-
-
-// syncChatproEventos().catch((error) => {
-//   console.error(`[${dataHora()}][index.js] Falha ao buscar sessões/mensagens: ${error.message}`);
+// inserirLeadsJetiMob().catch((error) => {
+//   console.error(`[${dataHora()}][index.js] Falha ao buscar Leads: ${error.message}`);
 //   process.exitCode = 1;
 // });
 
 
-//
+syncChatproEventos().catch((error) => {
+  console.error(`[${dataHora()}][index.js] Falha ao buscar sessões/mensagens: ${error.message}`);
+  process.exitCode = 1;
+});
